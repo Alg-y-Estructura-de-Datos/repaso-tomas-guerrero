bcf9f7a33d157ab1a834912ccba01063a5d3f0cb
tomasguerrero04 <tomas04guerrero@gmail.com>
2024-08-05 23:45:28 -0300

    Ejercicio 2 completo

--- Ej2.cpp
+++ Ej2.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int calcularFactorial(int);
+int calcularFactorialRecursivo(int);
+
+int main()
+{
 
     cout << "Examen Final Prog 3" << endl;
 
+    int n;
+    cout << " Ingrese el factorial que desea calcular: ";
+    cin >> n;
+
+    cout << " El factorial de " << n << " es " << calcularFactorial(n) << endl;
+    cout << " El factorial de " << n << " calculado de forma recursiva es: " << calcularFactorialRecursivo(n) << endl;
+
     return 0;
+}
+
+int calcularFactorial(int n)
+{
+    int factorialTotal = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        factorialTotal *= i;
+    }
+
+    return factorialTotal;
+}
+
+int calcularFactorialRecursivo(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * calcularFactorial(n - 1);
+    }
 }
