527b992f47d3a79b92082e2f0aaeddca12ad3ccc
tomasguerrero04 <tomas04guerrero@gmail.com>
2024-08-05 23:36:02 -0300

    Ejercicio 1 completo

--- Ej1.cpp
+++ Ej1.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void interCambioJugos(float &, float &);
+
+int main()
+{
 
     cout << "Examen Final Prog 3" << endl;
 
+    float jugoManzana, jugoNaranja;
+
+    cout << " Ingrese cuantos litros tiene el vaso con jugo de Manzana: ";
+    cin >> jugoManzana;
+    cout << " Ingrese cuantos litros tiene el vaso con jugo de Naranja: ";
+    cin >> jugoNaranja;
+
+    cout << " ANTES INTERCAMBIO " << endl;
+
+    cout << " El jugo de Naranja tiene " << jugoNaranja << " litros. " << endl;
+    cout << " El jugo de Manzana tiene " << jugoManzana << " litros. " << endl;
+
+    cout << " DESPUES INTERCAMBIO " << endl;
+    interCambioJugos(jugoManzana, jugoNaranja);
+
+    cout << " El jugo de Naranja tiene " << jugoNaranja << " litros. " << endl;
+    cout << " El jugo de Manzana tiene " << jugoManzana << " litros. " << endl;
     return 0;
+}
+
+void interCambioJugos(float &jugoManzana, float &jugoNaranja)
+{
+
+    float aux = jugoManzana;
+    jugoManzana = jugoNaranja;
+    jugoNaranja = aux;
 }
